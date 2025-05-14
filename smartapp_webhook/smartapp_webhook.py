53a77b8811f086a1c15e095383e9d5c1d98548ae
Егор <arepev2006@mail.ru>
2025-05-14 22:02:45 +0300

    оработан webhook: приём текста от ассистента и пересылка на сервер Dreamember

--- smartapp_webhook/smartapp_webhook.py
+++ smartapp_webhook/smartapp_webhook.py
@@ -1,30 +1,26 @@
-from flask import Flask, request, jsonify
 import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-# пока что хардкодим deviceID
-DEVICE_ID = "SberBoomSerialNumber"
 
 @app.route('/webhook', methods=['POST'])
 def handle_smartapp():
     data = request.json
     text = data["request"]["original_utterance"]
-    print(f"[SmartApp] Получено: {text}")
+    device_id = data["meta"]["client_id"]  # или временно: device_id = "91"
 
-    # Отправка на backend Димы
     payload = {
         "text": text,
-        "deviceID": DEVICE_ID
+        "deviceID": device_id
     }
-    response = requests.post("http://our_server_name.ru/api/dream", json=payload)
-    print(f"[Backend] Ответ от сервера Димы: {response.status_code}")
+
+    response = requests.post("https://dreamember.onrender.com/api/dream", json=payload)
 
     return jsonify({
         "version": data["version"],
         "session": data["session"],
         "response": {
-            "text": "Я записал твой сон!",
+            "text": "Сон записан!",
             "end_session": False
         }
     })
